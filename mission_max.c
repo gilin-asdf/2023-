ac4e3e06128f30d9766ab770c2ed75c2a9df55b6
gilin-asdf <129129663+gilin-asdf@users.noreply.github.com>
2023-05-11 01:19:14 +0000

    condition_swtich.c

---
+++ mission_max.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// main(){
+//   int a;
+//   int b;
+//   printf("정수2개를 입력하세요");
+//   scanf("%d, %d", &a , &b);
+
+//  if(a>b)
+//    printf ("%d", a);
+//  if(b>a)
+//    printf("%d", b);
+// }
